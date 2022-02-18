8b8acdfe0d46f5ab089ffd007d061f3dfaf36513
Eugene Nikolsky <e@egeek.me>
2022-02-18 12:40:03 +0200

    Reverse the input string
    As `localize` is a CLI target, adding a library for the `localize`
    function would be more trouble than creating a separate swift file and
    including it in the CLI and test targets, which is done here.

---
+++ localize/localize.swift
@@ -0,0 +1,15 @@
+//
+//  localize.swift
+//  localize
+//
+//  Created by eugenen on 18.02.2022.
+//
+
+import Foundation
+
+/**
+ Fake localization of an English string: reverses the string.
+ */
+public func localize(_ s: String) -> String {
+    String(s.reversed())
+}
