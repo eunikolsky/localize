9bbaa32b450fc6b45a2602ed16c158e24ba00a1a
Eugene Nikolsky <e@egeek.me>
2022-02-23 14:47:22 +0200

    Test that non-cased characters are preserved

--- test/Spec.hs
+++ test/Spec.hs
@@ -13,4 +13,7 @@ tests = testGroup "localize"
 
     , testCase "reverses and flips case of the input string" $
             localize "aBcD" @?= "dCbA"
+
+    , testCase "does not change non-cased characters" $
+            localize "0_- !?%8" @?= "8%?! -_0"
     ]
