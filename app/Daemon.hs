6e0c508a74759cc18f3baa526356e511e572a312
Eugene Nikolsky <e@egeek.me>
2022-05-27 09:12:09 +0300

    Write localized json result into a directory

--- app/Daemon.hs
+++ app/Daemon.hs
@@ -8,21 +8,23 @@ module Daemon
 
 import Control.Monad (forM_, unless)
 import Data.Aeson
+import Data.Map.Strict (Map)
 import Data.String (IsString)
 import GHC.Generics
-import System.Directory (doesFileExist, listDirectory)
+import System.Directory (doesFileExist, listDirectory, createDirectoryIfMissing)
 import System.Exit (die)
-import System.FilePath ((</>), takeExtension)
+import System.FilePath ((</>), replaceDirectory, takeDirectory, takeExtension)
 import System.FSNotify
 import System.FSNotify.Devel
 import System.IO (hPutStrLn, isEOF, stderr)
-import qualified Data.ByteString.Lazy.Char8 as C (putStrLn)
+import qualified Data.ByteString.Lazy.Char8 as C (ByteString, writeFile)
+import qualified Data.Map.Strict as M
 
 import JSON (localizeValue)
 
 -- | Daemon's config.
 newtype Config = Config
-  { watchDirs :: [FilePath] -- ^ Directories to watch.
+  { watchDirs :: Map FilePath FilePath -- ^ Directories to watch.
   }
   deriving Generic
 
@@ -39,8 +41,9 @@ parseConfig file = do
     Right config -> pure config
     Left err -> die $ "Error parsing config file: " ++ err
 
--- | Starts a daemon that monitors for changes in all json files in the given @watchDirs@
--- and localizes all the strings in the files on every change.
+-- | Starts a daemon that monitors for changes in all json files in the given @watchDirs@ (keys),
+-- localizes all the strings in the files on every change, and writes the result to the
+-- corresponding @watchDirs@ (values).
 startDaemon :: Config -> IO ()
 startDaemon (Config { watchDirs = dirs }) = do
   -- localize the files in the directory at startup to make sure we're up-to-date
@@ -49,7 +52,8 @@ startDaemon (Config { watchDirs = dirs }) = do
   -- FIXME this produces duplicate events for files in a subdirectory when both it and its parent
   -- directory are being watched.
   withManagerConf config $ \mgr -> do
-    forM_ dirs $ \dir -> treeExtExists mgr dir jsonExt localizeJSON
+    forM_ (M.toList dirs) $
+      \(dir, outputDir) -> treeExtExists mgr dir jsonExt (localizeJSON outputDir)
 
     waitForEOF
 
@@ -62,10 +66,10 @@ startDaemon (Config { watchDirs = dirs }) = do
     -- TODO check out the warning at https://www.stackage.org/haddock/lts-19.8/fsnotify-0.3.0.1/System-FSNotify.html#t:Debounce
     config = defaultConfig { confDebounce = Debounce 0.1 }
 
-    localizeAll = forM_ dirs $ \dir -> do
+    localizeAll = forM_ (M.toList dirs) $ \(dir, outputDir) -> do
       files <- listDirectory dir
       let jsons = fmap (dir </>) . filter ((== jsonExt) . takeExtension) $ files
-      mapM_ localizeJSON jsons
+      mapM_ (localizeJSON outputDir) jsons
 
 -- | Waits until the user hits @Ctlr+d@ (which is @stdin@'s EOF).
 waitForEOF :: IO ()
@@ -74,10 +78,17 @@ waitForEOF = do
   finished <- isEOF
   unless finished $ getLine >> waitForEOF
 
--- | Localizes all string values in the given @json@ file.
-localizeJSON :: FilePath -> IO ()
-localizeJSON file = do
+-- | Localizes all string values in the given json @file@ and writes the result to a file with
+-- the same name in @outputDir@.
+localizeJSON :: String -> FilePath -> IO ()
+localizeJSON outputDir file = do
   eitherValue <- eitherDecodeFileStrict' file
   case eitherValue of
-    Right value -> C.putStrLn . encode $ localizeValue value
+    Right value -> writeFile' (replaceDirectory file outputDir) . encode $ localizeValue value
     Left err -> hPutStrLn stderr err
+
+-- | Writes @ByteString@ to @file@ ensuring that its directory exists.
+writeFile' :: FilePath -> C.ByteString -> IO ()
+writeFile' file bs = do
+  createDirectoryIfMissing True $ takeDirectory file
+  C.writeFile file bs
