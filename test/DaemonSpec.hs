f8855cddd03dc090c06e497395172fd97d602816
Eugene Nikolsky <e@egeek.me>
2022-06-02 12:16:14 +0300

    Allow parallel tests in `DaemonSpec`
    This change removes the usage of `withCurrentDirectory` (a shared
    resource causing test failures) allowing the `tasty`'s default behavior
    of running tests in parallel. Unfortunately preparing `configText` is
    more complicated now because it needs to prepend the test directory
    before each directory.

--- test/DaemonSpec.hs
+++ test/DaemonSpec.hs
@@ -1,64 +1,87 @@
-{-# LANGUAGE QuasiQuotes #-}
-
 module DaemonSpec where
 
 import Control.Concurrent
 import Control.Concurrent.STM.TVar
 import Control.Monad
 import Control.Monad.STM
+import Data.Bifunctor
+import Data.List
 import Test.Tasty
 import Test.Tasty.ExpectedFailure
 import Test.Tasty.HUnit
-import Text.RawString.QQ (r)
 import System.Directory
+import System.FilePath
 
 import Daemon
 
-testDirectory :: FilePath
-testDirectory = "test/daemonspec"
+-- | Root directory where per-test case subdirectories are created.
+testRoot :: FilePath
+testRoot = "test/daemonspec"
 
 daemonTests :: TestTree
 daemonTests = testGroup "startDaemon"
   [ testCase "creates output directories" $ do
+      let testDirectory = testRoot </> "creates-output-directories"
       cleanDirectory testDirectory
-      withCurrentDirectory testDirectory $ do
-        mapM_ createDirectory ["dir", "lang", "lang/en"]
-        let configText = [r|
-          {"watchDirs": {
-            "dir": "out",
-            "lang": "fake",
-            "lang/en": "fake/en"
-          }}
-        |]
-        config <- readConfig configText
-        thread <- forkIO $ startDaemon localizeJSON config
+      let mapping = fmap (bimap (testDirectory </>) (testDirectory </>)) [("dir", "out"), ("lang", "fake"), ("lang/en", "fake/en")]
+      mapM_ (createDirectory . fst) mapping
+      let { configText = "{\"watchDirs\": {"
+        <> (intercalate "," . fmap (\(src, dest) -> wrapInQuotes src <> ":" <> wrapInQuotes dest) $ mapping)
+        <> "}}"
+      }
+      config <- readConfig testDirectory configText
+      thread <- forkIO $ startDaemon localizeJSON config
 
-        threadDelay 10000 -- TODO is 10 ms enough? refactor to a waiting call
-        assertDirectoriesExist ["out", "fake", "fake/en"]
+      threadDelay 10000 -- TODO is 10 ms enough? refactor to a waiting call
+      assertDirectoriesExist $ snd <$> mapping
 
-        killThread thread
+      killThread thread
 
-  , after AllFinish "creates" $
-    -- ^ this creates a dependency on the first test so that they don't run concurrently,
-    -- which fails because current directory is a shared resource
-    -- TODO refactor to remove 'withCurrentDirectory'
-      testCase "calls localize function only once on a file change (1)" $ do
-        cleanDirectory testDirectory
-        withCurrentDirectory testDirectory $ do
-          createDirectory "dir"
-          let configText = [r|
-            {"watchDirs": {
-              "dir": "out"
-            }}
-          |]
-          config <- readConfig configText
+  , testCase "calls localize function only once on a file change" $ do
+      let testDirectory = testRoot </> "calls-localize-once"
+      cleanDirectory testDirectory
+      let mapping = (testDirectory </> "dir", testDirectory </> "out")
+      createDirectory $ fst mapping
+      let { configText = "{\"watchDirs\": {"
+        <> wrapInQuotes (fst mapping) <> ":" <> wrapInQuotes (snd mapping)
+        <> "}}"
+      }
+      config <- readConfig testDirectory configText
+
+      callCountVar <- newTVarIO 0
+      thread <- forkIO $ startDaemon (fakeLocalizeJSON callCountVar) config
+
+      -- note: this initial delay is needed to skip the initial localization
+      threadDelay 1000
+      writeFile (testDirectory </> "dir/1.json") "{}"
+      -- 300 ms to wait for any watched localize calls in response to the file change
+      -- it's bigger than the debounce period of 100 ms in 'startDaemon'
+      threadDelay 300000
+
+      callCount <- readTVarIO callCountVar
+      callCount @?= 1
+
+      killThread thread
+
+  , expectFailBecause "there is a bug duplicate events from a nested dir" $
+    testGroup "when nested directories are watched"
+      [ testCase "calls localize function only once on a file change" $ do
+          let testDirectory = testRoot </> "nested_calls-localize-once"
+          cleanDirectory testDirectory
+          let mapping = fmap (bimap (testDirectory </>) (testDirectory </>)) [("dir", "out"), ("dir/nested", "out/nested")]
+          mapM_ (createDirectory . fst) mapping
+          let { configText = "{\"watchDirs\": {"
+            <> (intercalate "," . fmap (\(src, dest) -> wrapInQuotes src <> ":" <> wrapInQuotes dest) $ mapping)
+            <> "}}"
+          }
+          config <- readConfig testDirectory configText
 
           callCountVar <- newTVarIO 0
           thread <- forkIO $ startDaemon (fakeLocalizeJSON callCountVar) config
 
           -- note: this initial delay is needed to skip the initial localization
           threadDelay 1000
-          writeFile "dir/1.json" "{}"
+          writeFile (testDirectory </> "dir/nested/1.json") "[]"
           -- 300 ms to wait for any watched localize calls in response to the file change
           -- it's bigger than the debounce period of 100 ms in 'startDaemon'
           threadDelay 300000
@@ -67,39 +90,11 @@ daemonTests = testGroup "startDaemon"
           callCount @?= 1
 
           killThread thread
-
-  , after AllFinish "1" $
-      -- ^ mutually exclusive execution of all tests here; very dirty
-      expectFailBecause "there is a bug duplicate events from a nested dir" $
-      testGroup "when nested directories are watched"
-        [ testCase "calls localize function only once on a file change" $ do
-            cleanDirectory testDirectory
-            withCurrentDirectory testDirectory $ do
-              mapM_ createDirectory ["dir", "dir/nested"]
-              let configText = [r|
-                {"watchDirs": {
-                  "dir": "out",
-                  "dir/nested": "out/nested"
-                }}
-              |]
-              config <- readConfig configText
-
-              callCountVar <- newTVarIO 0
-              thread <- forkIO $ startDaemon (fakeLocalizeJSON callCountVar) config
-
-              -- note: this initial delay is needed to skip the initial localization
-              threadDelay 1000
-              writeFile "dir/nested/1.json" "[]"
-              -- 300 ms to wait for any watched localize calls in response to the file change
-              -- it's bigger than the debounce period of 100 ms in 'startDaemon'
-              threadDelay 300000
-
-              callCount <- readTVarIO callCountVar
-              callCount @?= 1
-
-              killThread thread
-        ]
+      ]
   ]
+
+wrapInQuotes :: String -> String
+wrapInQuotes s = "\"" ++ s ++ "\""
 
 -- | Counts the calls to the 'localizeJSON' function.
 fakeLocalizeJSON :: TVar Int -> LocalizeJSON
@@ -114,12 +109,13 @@ cleanDirectory :: FilePath -> IO ()
 cleanDirectory dir = do
   exists <- doesPathExist dir
   when exists $ removeDirectoryRecursive dir
-  createDirectory dir
+  let createParents = True
+  createDirectoryIfMissing createParents dir
 
--- | Saves the @config@ string into a file to parse it with @parseConfig@
+-- | Saves the @config@ string into a file in @dir@ to parse it with @parseConfig@
 -- and return a @Config@.
-readConfig :: String -> IO Config
-readConfig config = do
-  let configFile = "testconfig.json"
+readConfig :: FilePath -> String -> IO Config
+readConfig dir config = do
+  let configFile = dir </> "testconfig.json"
   writeFile configFile config
   parseConfig configFile
